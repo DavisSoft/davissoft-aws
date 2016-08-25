7b39d3a65554a1bc03ba74a2507ca84e88febb99
bronzelegs <tdavis0525@gmail.com>
2016-08-24 20:13:50 -0700

    updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('Copyright (C) Terrance Davis 2015');
+'use strict';
+
+(function() {
+  console.log('D a v i s S o f t . c o m');
+  console.log('all ur cod r us');
+  console.log('Copyright (C) Terrance Davis 2015, 2016');
+  console.log('scaffolding by generator-webapp 2.0');
+})();
